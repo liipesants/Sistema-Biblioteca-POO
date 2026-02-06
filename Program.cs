ad3fc52086dfc1d4fddb7a398a7e70629f68607f
Felipe Santos <felipesotnas2004@gmail.com>
2026-02-06 13:18:50 -0300

    Estrutura inicial do projeto

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GestaoBiblioteca
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
